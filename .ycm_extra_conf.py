91c895adea15011e95f3d547ec79eae373551fd0
Oleg Skoromnik <olegskor@gmail.com>
2018-11-09 15:47:16 +0100

    fixed python path YouCompleteMe

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -43,7 +43,7 @@ flags = [
     '-I..',
     '-I./src',
     local_include,
-    '-I/usr/local/Cellar/python3/3.7.0/Frameworks/Python.framework/Versions/3.7/include/python3.7m',
+    '-I/usr/local/Cellar/python3/3.7.1/Frameworks/Python.framework/Versions/3.7/include/python3.7m',
     '-I/usr/local/include',
     '-std=gnu++14',
 ]
